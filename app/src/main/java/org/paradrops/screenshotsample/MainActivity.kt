6c4619ddca00cc8fcc51eb1eb402801c082d23d1
Paraya <paraya3636@users.noreply.github.com>
2017-02-16 19:57:13 +0900

    First commit

---
+++ app/src/main/java/org/paradrops/screenshotsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.paradrops.screenshotsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ screenshot/src/main/java/org/paradrops/screenshot/Screenshot.kt
@@ -0,0 +1,4 @@
+package org.paradrops.screenshot
+
+class Screenshot {
+}
